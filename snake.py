5aba71c1f7167a24f5297ca5c23b96b92079fcb7
GlowingMan <cabreraruiz.emi@gmail.com>
2020-10-29 11:45:36 -0600

    updated snake.py

--- snake.py
+++ snake.py
@@ -1,36 +1,47 @@
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to arrow keys.
-
-"""
-
+# imports
 from turtle import *
-from random import randrange
+import random
 from freegames import square, vector
 
+# vectors that dictate the positions of the food, the snake, and where the snake goes
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+# dictionary that contains 5 different colors
+color = {1 : 'black', 2 : 'blue', 3 : 'green', 4 : 'orange', 5 : 'yellow'}	
+
+# variables that keep the key for the dictionary
+fKey = random.randint(1, 5)
+sKey = random.randint(1, 5)
+
+# makes sure that both keys are not the same
+while sKey == fKey:
+    sKey = random.randint(1, 5)
+
+# color variables
+foodC = color[fKey]
+snakeC = color[sKey]
+
+# moves the snake by changing the associated vector
 def change(x, y):
-    "Change snake direction."
     aim.x = x
     aim.y = y
 
+# checks the position of the head item to see whether it is within the boundaries
+# returns a boolean
 def inside(head):
-    "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+# the function that actually moves the snake
 def move():
-    "Move snake forward one segment."
+	# moves the snake itself
     head = snake[-1].copy()
     head.move(aim)
 
+	# checks the position of the head within the boundaries or inside 
+	# the snake's body to see if it's dead
+	# ends the game if ot's dead
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
@@ -38,44 +49,39 @@ def move():
 
     snake.append(head)
 
+	# checks the position of the head to see if it has eaten food, which then
+	# changes the food's position and elongates the snake
+	# if not, it eliminates the tail item to keep the snake the same size
     if head == food:
         print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
+        food.x = random.randrange(-15, 15) * 10
+        food.y = random.randrange(-15, 15) * 10
     else:
         snake.pop(0)
 
     clear()
 
+	# visualizes each item in the snake array with the color snakeC
     for body in snake:
-        square(body.x, body.y, 9, 'black')
-
-    square(food.x, food.y, 9, 'green')
+        square(body.x, body.y, 9, snakeC)
+	
+	# visualizes the food with the color foodC
+    square(food.x, food.y, 9, foodC)
     update()
     ontimer(move, 100)
 
+# start up the game, set up the space
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
+
+# does the associated lambda function when an arrow key is pressed
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+
+# the actual part that runs
 move()
 done()
- Logo
-
-Free Python Games
- 
-
-Donate
-If you or your organization uses Free Games, consider donating:
-
-Donate to Free Python Games
-
-Related Topics
-Documentation overview
-Previous: Guess
-Next: Crypto
-Quick search
